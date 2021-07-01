9e4d8879e99a609b1ba72e36a1634fe5c143da17
Cesar <cesare_ar@hotmail.com>
2021-07-01 11:17:54 -0500

    conversion de datos

---
+++ ConversionDatos/src/ConversionDatos.java
@@ -0,0 +1,25 @@
+
+public class ConversionDatos {
+
+	public static void main(String[] args) {
+		/*
+		 * Tipos de datos en Java
+		 * Numericos: byte, int, float, double, long, short
+		 * Booleanos: (verdadero, falso): boolean
+		 * Caracteres: char
+		 * Cadenas: String
+		 * Rodrigho1.aternos.me 
+		 */
+		byte numeropequeno = 122;
+		byte otronumeropequeno=122;
+		int suma= numeropequeno + otronumeropequeno;
+		int numero1=1000;
+		long numero2 =numero1;
+		float numero3= numero2;
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(suma);
+	}
+
+}
